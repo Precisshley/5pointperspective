438fd8530cc18e9840c7c0bd2796256f3717740f
Ashley Anderson <ashley.a1735@gmail.com>
2023-10-17 19:55:55 -0600

    Major fixes. TODO added

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# 5pointperspective
-Guidelines for drawing in 5 point perspective.
+# 5pointperspective in p5js
+Guidelines for drawing in 5 point perspective. Inspired by the functionality of an Etch-A-Sketch.
+
+There are 4 components to the guidelines.
+
+1. The main circle: Sets the size of the interior guidelines. Scrollwheel adjusts the size.
+2. z-line: For drawing lines at the angle from the center of the canvas to the mouse. Hold the 'z' key to lock its postion and start drawing on the line.
+3. x-line: For drawing lines along the x-axis ellipse (width = main circle). Hold the 'x' key to lock its postion and start drawing on the line.
+4. y-line: For drawing lines along the y-axis ellipse (height = main circle). Hold the 'y' key to lock its postion and start drawing on the line.
+
+Other controls:
+'p' = toggle black or white drawing
+'r' = reset
+'h' = hide guidelines
+'l' = toggle background colour (supposed to be night/light mode but it's terrible)
--- sketch.js
+++ sketch.js
@@ -1,32 +1,41 @@
 
-let bep;
-let pppmouseX, pppmouseY, ppmouseX, ppmouseY, ppointx, ppointy;
-//let witch;
+let drawingCanvas; //the canvas drawing vectors are attached to
+let pppmouseX, pppmouseY, ppmouseX, ppmouseY, ppointx, ppointy; //change x and y's to class
 let eraserOrPen = 255;
 let darkLightMode = 0;
 let release = 1;
-let s = 700;
+let circleSize = 700;
+let halfCircle = circleSize/2;
+
+let a, b, aM, bM, zAxisX, zAxisY  = 1;
+//a and b are the width and height of the ellipse guidelines
+//aM and bM are the actual values being saved for the drawing calculations.
+//zAxisX and zAxisY is the cordinate for drawing the z-guideline from the middle to the edge of the canvas
 
 function setup() { 
   if(windowHeight < windowWidth){
-         bep = createGraphics(windowHeight, windowHeight);
+         drawingCanvas = createGraphics(windowHeight, windowHeight);
   createCanvas(windowHeight, windowHeight);
      } else {
-           bep = createGraphics(windowWidth, windowWidth);
+           drawingCanvas = createGraphics(windowWidth, windowWidth);
   createCanvas(windowWidth, windowWidth);
      }
 }
 
-let a, b, c, d, e, f = 1;
-
 function mouseWheel(event) {
   let e = event.delta;
-  s = s+e;
+  circleSize = circleSize+e;
+  halfCircle = circleSize/2;
 }
 
 function draw() {
+  let pointx,pointy = 1; //drawing cursor position when locked onto guidelines
+
+  translate(width / 2, height / 2);
+
   //cursor(CROSS);
   noCursor();
+
   //screenshots
   // if (keyIsPressed && (key == 's') && (release == 0)) {
   //   saveFrame("/Users/ashleyanderson/Desktop/Processing Code/What Its Like To Chew 5 Gum/ScreenCaptures/screenshot-######.png");
@@ -34,6 +43,8 @@ function draw() {
   // }
   //screenshots
 
+
+  //dark/light mode
   if (keyIsPressed && (key == 'l') && (darkLightMode == 1) && (release == 0)) {
     darkLightMode = 0;
     release = 1;
@@ -42,16 +53,12 @@ function draw() {
     release = 1;
   } 
 
-  if (keyIsPressed && (key == 'p') && (eraserOrPen == 0) && (release == 0)) {
-    eraserOrPen = 255;
-    release = 1;
-  } else if (keyIsPressed && (key == 'p') && (eraserOrPen == 255) && (release == 0)) {
-    eraserOrPen = 0;
-    release = 1;
-  } 
-  if (keyIsPressed == false) {
-    release = 0;
+  if (darkLightMode == 0) {
+    background(0);
+  } else {  
+    background(255);
   }
+
   //eraserOrPen
   if (keyIsPressed && (key == 'p') && (eraserOrPen == 0) && (release == 0)) {
     eraserOrPen = 255;
@@ -63,53 +70,71 @@ function draw() {
   if (keyIsPressed == false) {
     release = 0;
   }
-  //eraserOrPen end
-  if (darkLightMode == 0) {
-    background(0);
-  } else {  
-    background(255);
-  }
+
+
   //draw
-  //bep.beginDraw();
-  bep.stroke(eraserOrPen);
-  let pointx = 1;
-  let pointy = 1;
+  //drawingCanvas.beginDraw();
+  
+  drawingCanvas.stroke(eraserOrPen); //sets to eraser for postioning stage
+
   if (keyIsPressed) {
-    if (key == 'x' || key == 'X') {
-      pointx = mouseX;
-      if (mouseY > height/2) {
-        pointy = b*sqrt(1-(pow(pointx-width/2, 2)/pow(a, 2)))+height/2;
+    
+      //draw z line [start]
+    if (key == 'z' || key == 'Z') {
+      if (PI/4<abs(atan(zAxisY/zAxisX))) {
+        pointy = (mouseY - (height/2));
+        pointx = (pointy/(zAxisY/zAxisX));
       } else {
-        pointy = -1*b*sqrt(1-(pow(pointx-width/2, 2)/pow(a, 2)))+height/2;
-      }
-    } else if (key == 'y' || key == 'Y') {
-      pointy = mouseY;
-      if (mouseX > width/2) {
-        pointx = d*sqrt(1-(pow(pointy-height/2, 2)/pow(c, 2)))+width/2;
-      } else {
-        pointx = -1*d*sqrt(1-(pow(pointy-height/2, 2)/pow(c, 2)))+width/2;
-      }
-    } else if (key == 'z' || key == 'Z') {
-      if (PI/4<abs(atan((f-(height/2))/(e-(width/2))))) {
-        pointy = mouseY;
-        pointx = ((pointy-(height/2))/((f-(height/2))/(e-(width/2))))+width/2;
-      } else {
-        pointx = mouseX;
-        pointy = (((f-(height/2))/(e-(width/2)))*(pointx-(width/2)))+(height/2);
+        pointx = (mouseX - (width/2));
+        pointy = ((zAxisY/zAxisX)*pointx);
       }
     }
-  } else {
-    pointx = mouseX;
-    pointy = mouseY;
+      //draw z line [end]
+    
+    else if (key == 'x' || key == 'X') { //calculates drawing on x axis ellipse
+      aM = halfCircle;
+      bM = a;
+      
+      let angle = atan2((mouseY - (height/2)) - 0, (mouseX - (width/2)) - 0);
+      
+      let radius = (aM) * (bM) / sqrt(pow(bM, 2) * pow(cos(angle), 2) + pow(aM, 2) * pow(sin(angle), 2));
+      
+      pointx = radius * cos(angle);
+      pointy = radius * sin(angle);
+      
+    }
+    else if (key == 'y' || key == 'Y') { //calculates drawing on y axis ellipse
+      aM = b;
+      bM = halfCircle;
+      
+      let angle = atan2((mouseY - (height/2)) - 0, (mouseX - (width/2)) - 0);
+      
+      let radius = (aM) * (bM) / sqrt(pow(bM, 2) * pow(cos(angle), 2) + pow(aM, 2) * pow(sin(angle), 2));
+      
+      pointx = radius * cos(angle);
+      pointy = radius * sin(angle);
+    }
+    
+  } else { //set cursor to mouse pos (no guideline lock on)
+    pointx = (mouseX - (width/2));
+    pointy = (mouseY - (height/2));
+  }
+  
+  if (!(keyIsPressed && (key == 'x' || key == 'X'))) { //calculate height of x ellipse
+    a = sqrt(pow((mouseY - (height/2)), 2)/(1-(pow((mouseX - (width/2)), 2)/pow(halfCircle, 2))));
+  }
+  if (!(keyIsPressed && (key == 'y' || key == 'Y'))) { //calculate width of y ellipse
+    b = sqrt(pow((mouseX - (width/2)), 2)/(1-(pow((mouseY - (height/2)), 2)/pow(halfCircle, 2)))); 
   }
 
 
-  bep.stroke(eraserOrPen);
+
+  drawingCanvas.stroke(eraserOrPen);
   if (mouseIsPressed) {
-    bep.noFill();
+    drawingCanvas.noFill();
 
     //stars
-    //draw.line(pointx, pointy, pointx, pointy);
+    // line(pointx, pointy, pointx, pointy);
 
     //draw.beginShape();
     //draw.curveVertex(pppmouseX, pppmouseY);
@@ -118,13 +143,12 @@ function draw() {
     //draw.curveVertex(pointx, pointy);
     //draw.endShape();
 
-
-    bep.beginShape();
-    bep.vertex(pppmouseX, pppmouseY);
-    bep.vertex(ppmouseX, ppmouseY);
-    bep.vertex(ppointx, ppointy);
-    bep.vertex(pointx, pointy);
-    bep.endShape();
+    drawingCanvas.beginShape(); //vertex doesnt translate like everything else
+    drawingCanvas.vertex(pppmouseX + (width/2), pppmouseY + (height/2));
+    drawingCanvas.vertex(ppmouseX + (width/2), ppmouseY + (height/2));
+    drawingCanvas.vertex(ppointx + (width/2), ppointy + (height/2));
+    drawingCanvas.vertex(pointx + (width/2), pointy + (height/2));
+    drawingCanvas.endShape();
   }
 
   pppmouseX = ppmouseX;
@@ -134,53 +158,38 @@ function draw() {
   ppointx = pointx;
   ppointy = pointy;
 
-  //reset
+
   if (keyIsPressed && (key == 'r' || key == 'R')) {
-    bep.clear();
+    drawingCanvas.clear();
   }
-  //reset end 
-  //bep.endDraw();
+  
+  image(drawingCanvas, width/-2, height/-2); //draws lines behind guidelines
 
-  image(bep, 0, 0);
-  //draw end
-
-  //ellipses
-
+  
+  
+  //guidelines [start]
   noFill();
-  stroke(125, 125, 125, 200);
-  //hide
-  if (keyIsPressed && (key == 'h' || key == 'H')) {
+  stroke(125, 125, 125, 200); //guideline colour
+  
+  if (keyIsPressed && (key == 'h' || key == 'H')) { //hides guidelines (needs to actually disappear. Not just turn black)
     stroke(125, 125, 125, 0);
   }
-  //hide
-  ellipse(pointx, pointy, 8, 8);
-  let size = s;
-  ellipse(width/2, height/2, size, size);
-  if (keyIsPressed && (key == 'x' || key == 'X')) {
-  } else {
-    a = size/2;
-    b = sqrt(pow(pointy-(width/2), 2)/(1-(pow(pointx-(height/2), 2)/pow(a, 2))));
-  }
-  ellipse(width/2, height/2, 2*a, 2*b);
+  
+  ellipse(pointx, pointy, 8, 8); //consider cross target with ellipses set to 80 in different axis
+  ellipse((mouseX - (width/2)), (mouseY - (height/2)), 3, 3);
+  ellipse(0, 0, circleSize, circleSize);
+  
+  ellipse(0, 0, circleSize, 2*a); // x ellipse guideline
 
-  if (keyIsPressed && (key == 'y' || key == 'Y')) {
-  } else {
-    c = size/2;
-    d = sqrt(pow(pointx-(height/2), 2)/(1-(pow(pointy-(width/2), 2)/pow(c, 2))));
+  ellipse(0, 0, 2*b, circleSize); // y ellipse guideline
+
+  if (!(keyIsPressed && (key == 'z' || key == 'Z'))) {
+    zAxisX = pointx*100;
+    zAxisY = pointy*100;
   }
-  ellipse(width/2, height/2, 2*d, 2*c);
-  //perspective line
-  if (keyIsPressed && (key == 'z' || key == 'Z')) {
-  } else {
-    e = pointx;
-    f = pointy;
-  }
-  line(0, ((f-(height/2))/(e-(width/2)))*(-width/2)+(height/2), width/2, height/2);
-  line(width, (((height/2)-f)/((width/2)-e))*(width/2)+(height/2), width/2, height/2);
-  //ellipses
+  
+  // line(0, (zAxisY/zAxisX), 0, 0);
+  line(zAxisX, zAxisY, 0, 0);
+  //guidelines [end]
+  
 }
-// TODO: 
-//option between curveVertex and vertex maybe also just speckles like it was
-//fix curve vertex glitch at edges pof ellipses
-//fill bucket
-//fix scrollwheel page zoom (use board code)
